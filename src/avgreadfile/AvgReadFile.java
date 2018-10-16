e517b6e9b85935b32d6f9f45b6b7f87494718310
conas1290 <conas1290@7HJL6Y1.Student.UGDSB.ED>
2018-10-16 15:07:52 -0400

    Finshed text reading

--- src/avgreadfile/AvgReadFile.java
+++ src/avgreadfile/AvgReadFile.java
@@ -6,42 +6,41 @@
 
 package avgreadfile;
 import java.io.*;
+import javax.swing.JOptionPane;
 
 public class AvgReadFile {
   public static void main(String[] args) throws IOException {
-
+    String myLine,fileName;
     /*Open the text file and attach a BufferedReader
-     * The path g:/randnums.txt must change depending where
-     * the file is located. */
+     */
+
+    fileName = JOptionPane.showInputDialog("Enter a name of a file: \n"
+            + "note files need to have their extention ie:.txt");
 
     BufferedReader readFile = new BufferedReader(
-       new FileReader("randnums.txt"));
-
-    int inMarks[] = new int[100];
-    String myLine;
-
-    System.out.println("Contents of the file randnums.txt:");
+       new FileReader(fileName));
+       System.out.println("Contents of the file "+fileName+":");
 
     //read 100 marks from the file randnums.txt
-    for (int i = 0; i < 100; i++) {
+    do {
       myLine = readFile.readLine();
+      if (myLine != null){
       System.out.println(myLine);
-      inMarks[i] = Integer.parseInt(myLine);
-    }
-
+      } else{}
+    } while(myLine != null);
     readFile.close();
 
-    System.out.print("The average of the random numbers is: ");
-    //Call findAverage method; passes values from inMark array
-    System.out.println(findAverage(inMarks));
+   // System.out.print("The average of the random numbers is: ");
+   //Call findAverage method; passes values from inMark array
+   //System.out.println(findAverage(inMarks));
   }
-
-  public static int findAverage(int marks[]) {
-    int sum = 0;
-    for (int i = 0; i < marks.length; i++) {
-      //sums the values in the new array 'marks'
-      sum += marks[i];	
-    }
-    return (sum / marks.length);	//returns average
-  }
+//testfile.txt
+//  public static int findAverage(int marks[]) {
+//    int sum = 0;
+//    for (int i = 0; i < marks.length; i++) {
+//      //sums the values in the new array 'marks'
+//      sum += marks[i];	
+//    }
+//    return (sum / marks.length);	//returns average
+//  }
 }
